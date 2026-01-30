f4cd58a9eb9f069cc0f387a4a3286eb005442643
55czdps6dj-ai <55czdps6dj@privaterelay.appleid.com>
2026-01-30 22:17:24 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// app.js
+let counts = {};
+let currentPage = 1;
+
+function init() {
+    // data.js の moveData を使って画面を作る処理
+    const list = document.getElementById('list-container');
+    // ...（中略：パーツ3で送ったロジック）
+}
+
+function updateCalc() {
+    // data.js の master 料金表を使って計算する処理
+    // ...
+}
+
+init();
